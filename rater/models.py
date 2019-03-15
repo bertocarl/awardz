01a936ffb0571c3a600b4dc7900e4c11169c4b5e
Albert Omware <aomware@gmail.com>
2019-03-15 23:33:44 +0300

    added new profile model

--- rater/models.py
+++ rater/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="", blank="")
+    image = models.ImageField(default='default.jpg', upload_to='profiles_pics')
+    bio = models.TextField(blank="", default="")
+    phonenumber = models.IntegerField(max_length=140, blank="",default="")
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
